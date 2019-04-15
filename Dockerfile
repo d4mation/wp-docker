0d6f42bcd9ce516fc884a969a1b17a10feb2915e
Eric Defore <eric.defore@gmail.com>
2019-04-15 17:55:42 -0400

    Maybe support Linux Hosts for xDebug?

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,10 @@ RUN docker-php-ext-enable xdebug
 
 RUN XDEBUG=$(find /usr/local/lib/php -name 'xdebug.so' | head -n 1 | tail -n 1) \
 	&& ln -s $XDEBUG /usr/local/lib/php/extensions/xdebug.so
+
+RUN apt-get update
+RUN apt-get install iproute -y
+
+COPY entrypoint.sh /custom-entrypoint.sh
+
+RUN chmod +x /custom-entrypoint.sh
---
+++ entrypoint.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Ensure we have the host.docker.internal hostname available to linux as well
+function fix_linux_internal_host() {
+  DOCKER_INTERNAL_HOST="host.docker.internal"
+
+  if ! grep $DOCKER_INTERNAL_HOST /etc/hosts > /dev/null ; then
+    DOCKER_INTERNAL_IP=`/sbin/ip route|awk '/default/ { print $3 }'`
+    echo -e "$DOCKER_INTERNAL_IP\t$DOCKER_INTERNAL_HOST" | tee -a /etc/hosts > /dev/null
+    echo 'Added $DOCKER_INTERNAL_HOST to hosts /etc/hosts'
+  fi
+}
+
+echo "Checking USER ID"
+www_uid=`stat -c "%u" /var/www/html`
+www_gid=`stat -c "%g" /var/www/html`
+
+echo "Host user is $www_uid:$www_gid"
+
+if [ ! $www_uid -eq 0 ]; then
+    fix_linux_internal_host
+fi
+
+exec "$@"
